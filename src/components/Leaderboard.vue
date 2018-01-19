04dcf4962a15f29ff7a24775ddc8b2504f19d60c
Kiran Anto <kirananto@gmail.com>
2018-01-19 07:35:05 +0530

    Do not Add script tags inside template

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -70,17 +70,6 @@ echo '<tr><th scope = "row" style="color: #00964d;" >'.($count).'</th><td>'.$r['
         <p class="text-xs-center">Copyright (c) 2017 Razorsharp.</p>
     </div>
 </footer>
-
-
-  <script src="assets/web/assets/jquery/jquery.min.js"></script>
-  <script src="assets/tether/tether.min.js"></script>
-  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/smooth-scroll/SmoothScroll.js"></script>
-  <script src="assets/dropdown/js/script.min.js"></script>
-  <script src="assets/touchSwipe/jquery.touchSwipe.min.js"></script>
-  <script src="assets/viewportChecker/jquery.viewportchecker.js"></script>
-  <script src="assets/jarallax/jarallax.js"></script>
-  <script src="assets/theme/js/script.js"></script>
   
   
   <input name="animation" type="hidden">
