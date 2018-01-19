503e08a23875aaafbc88760a1d37933fb0eb17ea
Kiran Anto <kirananto@gmail.com>
2018-01-19 07:34:55 +0530

    Set Routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,21 @@
 import Home from '@/components/Home'
 import Rules from '@/components/Rules'
+import Leaderboard from '@/components/Leaderboard'
+import EnterDetails from '@/components/EnterDetails'
+
 
 const routes = [
   { path: '/',
     component: Home
-  },
-  {
+  },{
     path: '/rules',
     component: Rules
+  },{
+    path: '/leaderboard',
+    component: Leaderboard
+  },{
+    path: '/EnterDetails',
+    component: EnterDetails
   }]
 
 export default routes
