c48e73aa9cf10ba04371055b30aea2bdbcff18d4
Kiran Anto <kirananto@gmail.com>
2018-01-24 16:38:24 +0530

    fix responsive design

--- src/components/404.vue
+++ src/components/404.vue
@@ -15,14 +15,12 @@
     <div class="container ">
         <div class="row">
             <div class="col-xs-12 col-md-12 text-xs-center">
-<div class="container" style=" padding-bottom: 70px; " >
-<div class="mbr-table-cell mbr-right-padding-md-up mbr-valign-top col-md-5 image-size" style="width: 50%;">
-                  <img src="/static/images/meme_404.jpg" class="img-rounded" alt=" Image appears here ">
-              </div>
-              <br><br>
-<router-link class="btn btn-primary btn-lg col-md-offset-2 col-md-8" to="/">GET ME OUT OF HERE</router-link>
-</div>
-       
+                <div class="container" style=" padding-bottom: 70px; " >
+                  <img src="/static/images/meme_404.jpg" class=" col-xs-12 offset-md-3" alt=" Image appears here ">
+                  <br><br>
+                  <router-link class="btn btn-primary btn-lg col-md-offset-2 col-md-8" to="/">GET ME OUT OF HERE</router-link>
+                </div>
+                       
             </div>
         </div>
     </div>
